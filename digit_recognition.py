c4c55c3d1675c77cc05b6ce1907fb842fd7c27f3
saloni <salonisharma1966@gmail.com>
2022-07-11 18:16:33 +0530

    mess

--- digit_recognition.py
+++ digit_recognition.py
@@ -1,18 +1,19 @@
 #Importing all the important models and install them if not installed on your device
 import cv2
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import numpy as np #array
+import pandas as pd #data analysis
+import seaborn as sns #data visualization
+import matplotlib.pyplot as plt #data visualization
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression #0 or 1
 from sklearn.metrics import accuracy_score
 #Python Imaging Library
 from PIL import Image
 import PIL.ImageOps
 #Secure Sockets Layer
 import os, ssl, time
+
 
 #Setting an HTTPS Context to fetch data from OpenML
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
@@ -45,16 +46,19 @@ cap = cv2.VideoCapture(0)
 
 while(True):
   # Capture frame-by-frame
+
   try:
     ret, frame = cap.read()
 
     # Our operations on the frame come here
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     #Drawing a box in the center of the video
+    
     height, width = gray.shape
-    upper_left = (int(width / 2 - 56), int(height / 2 - 56))
-    bottom_right = (int(width / 2 + 56), int(height / 2 + 56))
+    upper_left = (int(width / 2 - 100), int(height / 2 - 100))
+    bottom_right = (int(width / 2 + 100), int(height / 2 + 100))
 
     #source, width,height,color{green},width of rect
     cv2.rectangle(gray, upper_left, bottom_right, (0, 255, 0), 2)
@@ -73,13 +77,18 @@ while(True):
 
     image_bw_resized_inverted = PIL.ImageOps.invert(image_bw_resized)
     pixel_filter = 20
+
     min_pixel = np.percentile(image_bw_resized_inverted, pixel_filter)
     image_bw_resized_inverted_scaled = np.clip(image_bw_resized_inverted-min_pixel, 0, 255)
     max_pixel = np.max(image_bw_resized_inverted)
+
     image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
     test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,784)
+    
     test_pred = clf.predict(test_sample)
     print("Predicted class is: ", test_pred)
+
+
 
     # Display the resulting frame
     cv2.imshow('frame',gray)
